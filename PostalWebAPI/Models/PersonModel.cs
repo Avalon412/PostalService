74c79c1d6cbed356bd82d655570e1ecd82bb4f3a
Женя Василинчук <marakasi88887@gmail.com>
2022-01-22 20:22:26 +0200

    Add project files.

---
+++ PostalWebAPI/Models/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostalWebAPI.Models
+{
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
