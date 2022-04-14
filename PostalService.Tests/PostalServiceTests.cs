33854a631c7f0af242a6459e57234186c4e76e82
Женя Василинчук <marakasi88887@gmail.com>
2022-04-14 21:54:29 +0300

    Added Unit test

---
+++ PostalService.Tests/PostalServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using PostalService.DAL.Models;
+using PostalService.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FakeItEasy;
+using PostalService.Services.Services;
+
+namespace PostalService.Tests
+{
+    public class PostalServiceTests
+    {
+        [Fact]
+        public void OnCreatingPackage_PackageIsNull_ThrowsNullReferenceException()
+        {
+            //Arange
+            PackageModel package = null;
+            var packageService = A.Fake<PackageService>();
+            //Act
+            Func<Task> func = async() => await packageService.Create(package);
+            //Assert
+            Assert.ThrowsAsync<NullReferenceException>(func);
+        }
+    }
+}
